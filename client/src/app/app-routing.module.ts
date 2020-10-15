b2bd959fe23411b02c099e1e94a2612118705076
Aastha Desai <itw.aasthad@gmail.com>
2020-10-15 23:46:10 +0530

    End of section 7

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorsComponent } from './errors/server-errors/server-errors.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MeberDetailComponent } from './members/meber-detail/meber-detail.component';
@@ -24,6 +27,9 @@ const routes: Routes = [
       { path: 'messages', component: MessagesComponent },
     ],
   },
+  { path: 'errors', component: TestErrorsComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'server-errors', component: ServerErrorsComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
 
